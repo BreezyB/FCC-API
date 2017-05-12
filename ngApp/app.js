73936b3767d989cf0ffa4cdd7fdd4739de0ea961
Eboni Wigginton <eboni@wigginton.me>
2017-05-12 18:25:32 -0400

    initial commit

---
+++ ngApp/app.js
@@ -0,0 +1,24 @@
+var fccapi;
+(function (fccapi) {
+    angular.module('fccapi', ['ui.router', 'ngResource', 'ui.bootstrap']).config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $stateProvider
+            .state('home', {
+            url: '/',
+            templateUrl: '/ngApp/views/home.html',
+            controller: fccapi.Controllers.HomeController,
+            controllerAs: 'controller'
+        })
+            .state('about', {
+            url: '/about',
+            templateUrl: '/ngApp/views/about.html',
+            controller: fccapi.Controllers.AboutController,
+            controllerAs: 'controller'
+        })
+            .state('notFound', {
+            url: '/notFound',
+            templateUrl: '/ngApp/views/notFound.html'
+        });
+        $urlRouterProvider.otherwise('/notFound');
+        $locationProvider.html5Mode(true);
+    });
+})(fccapi || (fccapi = {}));
---
+++ ngApp/controllers/controllers.js
@@ -0,0 +1,20 @@
+var fccapi;
+(function (fccapi) {
+    var Controllers;
+    (function (Controllers) {
+        var HomeController = (function () {
+            function HomeController() {
+                this.message = 'Hello from the home page!';
+            }
+            return HomeController;
+        }());
+        Controllers.HomeController = HomeController;
+        var AboutController = (function () {
+            function AboutController() {
+                this.message = 'Hello from the about page!';
+            }
+            return AboutController;
+        }());
+        Controllers.AboutController = AboutController;
+    })(Controllers = fccapi.Controllers || (fccapi.Controllers = {}));
+})(fccapi || (fccapi = {}));
